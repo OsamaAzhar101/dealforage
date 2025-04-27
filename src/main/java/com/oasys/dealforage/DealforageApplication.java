5862a02d8ba34dba6abac89021a4adf62589852f
OsamaAzhar101 <osamaazhar591@gmail.com>
2025-04-27 14:08:45 +0400

    consume API's based on debounce scrolling parameteres

---
+++ src/main/java/com/oasys/dealforage/DealforageApplication.java
@@ -0,0 +1,13 @@
+package com.oasys.dealforage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealforageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealforageApplication.class, args);
+	}
+
+}
